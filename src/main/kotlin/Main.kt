a26a08253b471c2466d303551d449420abbf4938
Shmuel Sternbach <shmuelsternbach613@gmail.com>
2020-09-17 19:20:49 -0400

    Did not finish organizing hashmap into arrays of aleph-beis. We realized mid-way that arrays were faster and decided to perform a refactor. This is pre-refactor.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,64 +6,57 @@ private val listOfDelimiters = mutableListOf<Char>(' ', '.', ',', ';', ':', '\''
 private val listOfEnglishLettersAndForbiddenCharacters = listOfDelimiters + ('A'..'Z') + ('a'..'z')
 fun main() {
     val timeToComplete = measureNanoTime {
-        val folder = File("C:\\Users\\Shmuel\\OneDrive\\IdeaProjects\\FindMostCommonSefariaWords\\Tosafot")
-//        val folder = File("C:\\Users\\Shmuel\\OneDrive\\Desktop\\Sefaria Texts (Excluding Tanach, Talmud, and Tanaitic texts)\\Relevant categories")
-        val filesInFolder = folder.walk().filter { it.name == "merged.txt" }.toList()
-        val mutableListOfStrings = mutableListOf<String>()
-        for (file in filesInFolder) {
-            val bufferedReader = file.bufferedReader()
-            mutableListOfStrings.add(bufferedReader.readText())
-            bufferedReader.close()
-        }
-        /*stringBuilder.filter {
-        it in listOfEnglishLettersAndForbiddenCharacters
-    }*/
-/*    val bufferedReader = File("C:\\Users\\Shmuel\\OneDrive\\Desktop\\Sefaria Texts (Excluding Tanach, Talmud, and Tanaitic texts)\\Relevant categories\\Halakhah\\Shulchan Arukh\\Shulchan Arukh, Orach Chayim\\Hebrew\\merged.txt").bufferedReader()
-    val bookText = bufferedReader.readText()
-    val stringBuilder1 = StringBuilder(bookText)*/
-//            text.removeRange(0, "(?<=\n)[\\p{InHebrew}\\s.,;:\'\"!?</>\\\\]+(?=\n)".toRegex().find(text)?.value?.indices?.first?:0)
-
-        /*  stringBuilder1.filter {
-        it in listOfEnglishLettersAndForbiddenCharacters
-    }*/
-//    val text = "hello hello hello hello hello hello hello hello one one one one a b n a a a a a a a a a a a a a a a a a ab b b b b a a a a"
+        val numberOfLettersInAlphabet = 22//22 letters in aleph-beis
+        val mutableListOfStrings = getMutableListOfStrings(File("C:\\Users\\Shmuel\\OneDrive\\IdeaProjects\\FindMostCommonSefariaWords\\Tosafot"))
+        //restrict any entries with forbidden characters:
         mutableListOfStrings.removeIf { it1 -> it1.none { it2 -> it2 in listOfEnglishLettersAndForbiddenCharacters } }
-//    for(string in mutableListOfStrings){
-        var stringBuilderCounter = 0
-        val mutableListOfStringBuilders = mutableListOf(StringBuilder())
-        var flattenedString = mutableListOfStringBuilders[stringBuilderCounter]
-//        var flattenedString = StringBuilder()
-        mutableListOfStrings.forEach {
-            if (flattenedString.length + it.length >= 33_000) flattenedString.append(it) else {
-                stringBuilderCounter++
-                mutableListOfStringBuilders.add(StringBuilder())
-                flattenedString = mutableListOfStringBuilders[stringBuilderCounter]
-                flattenedString.append(it)
+
+        val mutableListOfStringBuilders = getMutableListOfStringBuilders(mutableListOfStrings)
+
+        val listOfMapsOfWordsMappedToFrequency = getListOfFrequencyMapsFromListOfStringBuilders(/*string*//*flattenedString.toString()*/mutableListOfStringBuilders)
+//        println(filesInFolder[mutableListOfStrings.indexOf(flattenedString/*string*/)])
+//        println(wordsMappedToFrequencySortedByValue)
+
+        val mutableListOfWordMapsGroupedByLetter = mutableListOf<MutableMap<String, Int?>>()
+        for (i in 1..numberOfLettersInAlphabet) mutableListOfWordMapsGroupedByLetter.add(mutableMapOf())
+
+        val alphabetList = listOf('א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת')
+        var indexOfAlephBeisList = 0
+        for (frequencyMap in listOfMapsOfWordsMappedToFrequency) {
+            for ((word, count) in frequencyMap) {
+                while (indexOfAlephBeisList < alphabetList.size) {
+                    if (word.first() == alphabetList[indexOfAlephBeisList]) {
+                        for (mutableMap in mutableListOfWordMapsGroupedByLetter) {
+                            if(word in mutableMap){
+                                if (count != null) {
+                                    mutableMap[word] = mutableMap[word]?.plus(count)
+                                //TODO at this point we realized that arrays were faster. goodby forever. hopefully.
+                                }
+                            }
+                        }
+                        println("do something")
+                    }
+                    indexOfAlephBeisList++
+                }
+
             }
         }
 
-        val listOfMapsOfWordsMappedToFrequencySortedByValue = getListOfSortedFrequencyMapsFromListOfStringBuilders(/*string*//*flattenedString.toString()*/mutableListOfStringBuilders)
-//        println(filesInFolder[mutableListOfStrings.indexOf(flattenedString/*string*/)])
-//        println(wordsMappedToFrequencySortedByValue)
-        val textFile = File("output.txt")
-        textFile.createNewFile()
-        val bw = textFile.bufferedWriter()
-        listOfMapsOfWordsMappedToFrequencySortedByValue.forEach { map -> map.forEach { entry -> bw.append("${entry.key.prependIndent("\n")}=${entry.value}") } }
+        listOfMapsOfWordsMappedToFrequency.forEach { map ->
+            map.forEach { entry ->
+                println("${entry.key.prependIndent("\n")}=${entry.value}")
+            }
+        }
 //    wordsMappedToFrequencySortedByValue.forEach { print("${it.key.prependIndent("\n")}=${it.value}") }
 //    println()
-        bw.close()
-        val listOfCondensedFrequencyMaps = mutableListOf<Map<String, Int>>()
         //remove cross-map duplicates so that each map contains unique words
-        //results in a situation where you will potentially have the same amount of maps as the beggining, but each map will (only contain words which are in no other map and) just contain a few words. The next step is to fill the maps sequentially until you get to the last map. values to
-        for (map1: MutableMap<String, Int?> in listOfMapsOfWordsMappedToFrequencySortedByValue) {
-            for (map2: MutableMap<String, Int?> in listOfMapsOfWordsMappedToFrequencySortedByValue.subList(listOfMapsOfWordsMappedToFrequencySortedByValue.indexOf(map1),listOfMapsOfWordsMappedToFrequencySortedByValue.size)) {
+        for (map1: MutableMap<String, Int?> in listOfMapsOfWordsMappedToFrequency) {
+            for (map2: MutableMap<String, Int?> in listOfMapsOfWordsMappedToFrequency.subList(listOfMapsOfWordsMappedToFrequency.indexOf(map1) + 1, listOfMapsOfWordsMappedToFrequency.size)) {
                 for ((key, value) in map2.toMap()) {
                     if (key in map1) {
                         if (value != null) {
                             map1[key] = map1[key]?.plus(value)
                             map2.remove(key)
-                            //add key's associated value (its word count) as recorded in map2 to key's value in map1
-                            //delete/remove key from map2
                         }
                     }
                 }
@@ -75,11 +68,39 @@ fun main() {
             wordsMappedToFrequency = mutableListOfMaps[hashMapCounter]
             wordsMappedToFrequency.incrementCountOrSetToOne(word)
         }*/
+
     }
     println("Seconds to complete: ${timeToComplete / 1_000_000_000}")
 }
 
-private fun getListOfSortedFrequencyMapsFromListOfStringBuilders(/*text: String*/listOfStringBuilders: MutableList<StringBuilder>): MutableList<MutableMap<String, Int?>> {
+private fun getMutableListOfStringBuilders(mutableListOfStrings: MutableList<String>): MutableList<StringBuilder> {
+    var stringBuilderCounter = 0
+    val mutableListOfStringBuilders = mutableListOf(StringBuilder())
+    var flattenedString = mutableListOfStringBuilders[stringBuilderCounter]
+    mutableListOfStrings.forEach {
+        if (flattenedString.length + it.length >= 33_000) flattenedString.append(it) else {
+            stringBuilderCounter++
+            mutableListOfStringBuilders.add(StringBuilder())
+            flattenedString = mutableListOfStringBuilders[stringBuilderCounter]
+            flattenedString.append(it)
+        }
+    }
+    return mutableListOfStringBuilders
+}
+
+private fun getMutableListOfStrings(folder: File): MutableList<String> {
+//        val folder = File("C:\\Users\\Shmuel\\OneDrive\\Desktop\\Sefaria Texts (Excluding Tanach, Talmud, and Tanaitic texts)\\Relevant categories")
+    val filesInFolder = folder.walk().filter { it.name == "merged.txt" }.toList()
+    val mutableListOfStrings = mutableListOf<String>()
+    for (file in filesInFolder) {
+        val bufferedReader = file.bufferedReader()
+        mutableListOfStrings.add(bufferedReader.readText())
+        bufferedReader.close()
+    }
+    return mutableListOfStrings
+}
+
+private fun getListOfFrequencyMapsFromListOfStringBuilders(/*text: String*/listOfStringBuilders: MutableList<StringBuilder>): MutableList<MutableMap<String, Int?>> {
     val mutableListOfMaps = mutableListOf(HashMap<String, Int?>())
     val newList = mutableListOf<MutableMap<String, Int?>>()
     for (stringBuilder in listOfStringBuilders) {
@@ -105,7 +126,7 @@ private fun getListOfSortedFrequencyMapsFromListOfStringBuilders(/*text: String*
         }
 //    val wordsSorted = mutableListOfWords.sorted().toMutableList()
         mutableListOfMaps.forEach { hashMap ->
-            newList.add(hashMap.toList().sortedBy { pair -> pair.second }.reversed().toMap().toMutableMap())
+            newList.add(hashMap.toList()/*.sortedBy { pair -> pair.second }.reversed()*/.toMap().toMutableMap())
         }
 //    val wordsMappedToFrequencySortedByValue = wordsMappedToFrequency.toList().sortedBy { it.second }.reversed().toMap()
 //    return wordsMappedToFrequencySortedByValue
@@ -144,3 +165,12 @@ private fun getSortedFrequencyMapOfThreeMostCommon(text: String): Map<String, In
 }
 
 private fun HashMap<String, Int?>.incrementCountOrSetToOne(word: String) = if (containsKey(word)) this[word] = this[word]?.plus(1) else this[word] = 1
+
+fun Map<String, Int?>.organizeByLetter() {
+    val x = 5
+    val new = x + 1
+}
+
+fun addOneToNumber(int: Int): Int {
+    return int + 1
+}
--- src/main/kotlin/RandomTests3.kt
+++ src/main/kotlin/RandomTests3.kt
@@ -1,6 +1,35 @@
 fun main() {
-    println(sigma(3))
+//    println(sigma(3))
 }
 fun sigma(int:Int): Int {
     return (1..int).fold(0,Int::plus)
+}
+private fun getSortedFrequencyMap(text: String/*listOfStringBuilders: MutableList<StringBuilder>*/): Map<String, Int?> {
+    val listOfWords = text.split(" ", ".", ",", ";", ":", /*"\"",*/ "\'", "!", "?", "<", ">", "/", "\\", "%", "$", "#", "@", "^", "&", "*", "(", ")", "-", "+", "=", "{", "[", "]", "}", "`", "~", "_", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0").filterNot { it == ""/*||it.length<3*/}
+    val mutableListOfWords = MutableList(listOfWords.size) {
+        listOfWords[it]
+    }
+
+    val wordsMappedToFrequency = HashMap<String, Int?>()
+    var firstMapFull = false
+    var positionInListOfHashMaps = 0
+    //wordsMappedToFrequency = ListOfHashMaps[positionListOfHashMaps]
+    mutableListOfWords.forEach { word ->
+        if (wordsMappedToFrequency.size != 1073741823) {//if hashmap is not at max_capacity-1 (just for safety)
+            if (wordsMappedToFrequency.containsKey(word)) {
+                val count = wordsMappedToFrequency[word]!!
+                wordsMappedToFrequency[word] = count + 1
+            } else {
+                wordsMappedToFrequency[word] = 1
+            }
+        } else{
+            println("HASHMAP reached max capacity!!!")
+            firstMapFull = true
+            //insert code to add a new map to list
+            positionInListOfHashMaps++
+        }
+    }
+//    val wordsSorted = mutableListOfWords.sorted().toMutableList()
+    val wordsMappedToFrequencySortedByValue = wordsMappedToFrequency.toList().sortedBy { it.second }.reversed().toMap()
+    return wordsMappedToFrequencySortedByValue
 }
